7f59551ed5e11160bf4bbb036ae808fca2e0631b
Jared Jensen <jared.jensen@forgerock.com>
2020-11-15 11:53:55 -0800

    Adds compiler directives and wifi

---
+++ sender/app/WiFiCreds.example.h
@@ -0,0 +1,2 @@
+#define WIFI_SSID "my-wifi-ssid"
+#define WIFI_PASS "my-wifi-password"
--- sender/app/app.ino
+++ sender/app/app.ino
@@ -1,33 +1,86 @@
-const float fullCharge = 12.8;
-const int ledPin = 13;
+#include <WiFi101.h>
+#include "WiFiCreds.h"
 
-// Specify the resistors and board voltage to use
-const float r1 = 1000000, r2 = 220000;
-const float boardVoltage = 5.0;
+#define DEBUG
+
+// Battery settings
+#define FULL_CHARGE 12.8
+#define CHECK_INTERVAL 2000
+
+// Board settings
+#define R1 1000000.0
+#define R2 220000.0
+#define BOARD_VOLTAGE 3.3
+#define LED_PIN 12
+#define WIFI_CHIPSELECT 8
+#define WIFI_IRQ 7
+#define WIFI_RESET 4
+#define WIFI_ENABLE 2
+
+int wifiStatus = WL_IDLE_STATUS;
 
 void setup() {
+  initDebug();
   debug("Starting");
-  initDisplay();
   initLED();
+  initWiFi();
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);
-  float voltage = calcVoltage(sensorValue);
+  float voltage = getVoltage();
   displayVoltage(voltage);
-  delay(2000);
+  reportVoltage(voltage);
+  delay(CHECK_INTERVAL);
 }
 
-void debug(String text) {
-  if (Serial) Serial.println(text);
+void debug(char text[]) {
+#ifdef DEBUG
+  Serial.println(text);
+#endif
+}
+
+void debug(float val) {
+#ifdef DEBUG
+  Serial.println(val);
+#endif
+}
+
+void initDebug() {
+#ifdef DEBUG
+  Serial.begin(9600);
+  while (!Serial);
+#endif
 }
 
 void initLED() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void initWiFi() {
+  debug("Initializing WiFi");
+  WiFi.setPins(WIFI_CHIPSELECT, WIFI_IRQ, WIFI_RESET, WIFI_ENABLE);
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    debug("WiFi shield not present");
+    while (true);
+  }
+  debug("WiFi shield detected");
+
+  while (wifiStatus != WL_CONNECTED) {
+    debug("Attempting to connect to WiFi");
+    debug(WIFI_SSID);
+    wifiStatus = WiFi.begin(WIFI_SSID, WIFI_PASS);
+    Serial.println(wifiStatus);
+    flashLED(2);
+    delay(5000);
+  }
+
+  debug("Connected to WiFi");
+  flashLED(4);
 }
 
 void setLED(bool on) {
-  digitalWrite(ledPin, on ? HIGH : LOW);
+  digitalWrite(LED_PIN, on ? HIGH : LOW);
 }
 
 void flashLED(int count) {
@@ -41,26 +94,22 @@ void flashLED(int count) {
   }
 }
 
-void initDisplay() {
-  if (!Serial) return;
-  Serial.begin(9600);
-}
-
-float calcVoltage(float sensorValue) {
-  return sensorValue * (boardVoltage / 1023.0) / (r2 / (r1 + r2));
+float getVoltage() {
+  int sensorValue = analogRead(A0);
+  return float(sensorValue) * (BOARD_VOLTAGE / 1024.0) / (R2 / (R1 + R2));
 }
 
 void displayVoltage(float voltage) {
-  if (voltage < fullCharge * 0.6) {
+  if (voltage < FULL_CHARGE * 0.6) {
     flashLED(3);
-  } else if (voltage < fullCharge * 0.8) {
+  } else if (voltage < FULL_CHARGE * 0.8) {
     flashLED(2);
   } else {
     flashLED(1);
   }
-  if (Serial) Serial.println(voltage);
+  debug(voltage);
 }
 
 void reportVoltage(float voltage) {
-  // todo: call remote api
+  debug("TODO: Report voltage");
 }
